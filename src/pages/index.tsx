269fd963a637487ca22eec5bd14fb950a60ffcc3
Gabriel Fagundes Oczust <gabriel.oczust@meta.com.br>
2021-11-29 14:11:16 -0300

    refactor: create src folder

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <header></header>
+      <main></main>
+      <footer></footer>
+    </div>
+  );
+};
+
+export default Home;
