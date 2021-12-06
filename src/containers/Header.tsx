d17c651a39285409b25613126edcf3456c6ec96d
renatogual <renatogual@hotmail.com>
2021-12-05 22:41:53 -0300

    feat: add header navigation

--- src/containers/Header.tsx
+++ src/containers/Header.tsx
@@ -1,8 +1,23 @@
+import Image from "next/image";
+import logo from "../assets/images/eng_seg_logo.png";
+
+import styles from "./Header.module.css";
+
 const Header = () => {
   return (
     <header>
       <div className="container">
-        <div className="header"></div>
+        <nav className={styles.nav}>
+          <Image src={logo} alt="logo" width={125} height={100} />
+          <div>
+            <a href="#quemSomos">Quem Somos</a>
+            <a href="#treinamentos">Treinamentos</a>
+            <a href="#serviços">Serviços</a>
+            <a href="#consultorias">Consultorias</a>
+            <a href="#clientes">Clientes</a>
+            <a href="#contato">Contato</a>
+          </div>
+        </nav>
       </div>
     </header>
   );
