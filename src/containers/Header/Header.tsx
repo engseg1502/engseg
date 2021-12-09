782930eed13217dac11a66dc66d58b9f16cac6f2
Gabriel Fagundes Oczust <gabriel.oczust@meta.com.br>
2021-12-09 20:04:33 -0300

    fix(Header): mobile styles

--- src/containers/Header/Header.tsx
+++ src/containers/Header/Header.tsx
@@ -1,25 +1,58 @@
+import { useState } from "react";
+
+import Link from "next/link";
 import Image from "next/image";
 import styles from "./Header.module.css";
 
 const Header = () => {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  const handleClickHamburger = () => {
+    setMenuIsOpen((old) => !old);
+  };
+
+  const handleClickItemMenu = () => {
+    setMenuIsOpen(false);
+  };
+
   return (
     <header className={styles.header}>
-      <div className="container">
+      <div className={`container ${styles.container}`}>
         <nav className={styles.nav}>
-          <Image
-            src="/images/eng_seg_logo.png"
-            alt="logo"
-            width={125}
-            height={88}
-          />
+          <Link href="/" passHref>
+            <div className={styles.logo}>
+              <Image
+                src="/images/eng_seg_logo.png"
+                alt="logo"
+                width={125}
+                height={88}
+              />
+            </div>
+          </Link>
 
-          <div className={styles.menu}>
-            <a href="#quemSomos">Quem Somos</a>
-            <a href="#treinamentos">Treinamentos</a>
-            <a href="#serviços">Serviços</a>
-            <a href="#consultorias">Consultorias</a>
-            <a href="#clientes">Clientes</a>
-            <a href="#contato">Contato</a>
+          <div className={styles.hamburguer} onClick={handleClickHamburger}>
+            <span></span>
+          </div>
+
+          <div className={styles.menu} data-open={menuIsOpen}>
+            <a onClick={handleClickItemMenu} href="#quemSomos">
+              Quem Somos
+            </a>
+            <a onClick={handleClickItemMenu} href="#treinamentos">
+              Treinamentos
+            </a>
+            <a onClick={handleClickItemMenu} href="#serviços">
+              Serviços
+            </a>
+            <a onClick={handleClickItemMenu} href="#consultorias">
+              Consultorias
+            </a>
+            <a onClick={handleClickItemMenu} href="#clientes">
+              Clientes
+            </a>
+            <a onClick={handleClickItemMenu} href="#contato">
+              Contato
+            </a>
           </div>
         </nav>
       </div>
