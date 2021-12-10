6227232729f271a56111b17f8b04aff9a4bd4dab
Gabriel Fagundes Oczust <gabriel.oczust@meta.com.br>
2021-12-10 11:53:31 -0300

    feat: copyright

--- src/containers/Footer/Footer.tsx
+++ src/containers/Footer/Footer.tsx
@@ -1,14 +1,23 @@
 import styles from "./Footer.module.css";
 
 const Footer = () => {
+  const year = new Date().getFullYear();
+
   return (
     <footer className={styles.footer}>
       <div className="container">
         <div className={styles.copyright}>
-          <p className={styles.text}>
-            {`© ${new Date().getFullYear()} `}Eng.Seg | Assessoria e
-            Treinamentos <i>-</i> <span>Todos os Direitos Reservados</span>
-          </p>
+          <span>
+            © {year} | <b>Eng.Seg</b> | <br /> Todos os direitos reservados
+          </span>
+
+          <a
+            href="https://api.whatsapp.com/send?phone=5541999023899&text=Ol%C3%A1%20entro%20contato%20atav%C3%A9s%20do%20site%20da%20EngSeg,%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Desenvolvido por Gabriel Oczust
+          </a>
         </div>
       </div>
     </footer>
