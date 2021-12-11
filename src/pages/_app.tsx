bddc5f7966aa45dc7eed5549a09ca8245a204c41
Gabriel Fagundes Oczust <gabriel.oczust@meta.com.br>
2021-12-11 00:21:25 -0300

    seo: meta tags and favicons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,46 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
+
 import { Header, Footer } from "containers";
 import { Whatsapp } from "components";
+
 import "styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Eng.Seg - Assessoria e Treinamentos</title>
+        <link rel="canonical" href="https://www.engseg.art.br/" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="robots" content="index, follow" />
+        <meta name="author" content="Gabriel Oczust" />
+        <meta name="keywords" content=""></meta>
+        <meta
+          name="description"
+          content="Atuamos no segmento de treinamento e assessoria em segurança, meio ambiente e saúde industrial, com foco em aplicação de soluções para a empresa"
+        />
+        <meta property="og:locale" content="pt_BR" />
+        <meta
+          property="og:title"
+          content="Eng.Seg - Assessoria e Treinamentos"
+        />
+        <meta
+          property="og:site_name"
+          content="Eng.Seg - Assessoria e Treinamentos"
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:description"
+          content="Atuamos no segmento de treinamento e assessoria em segurança, meio ambiente e saúde industrial, com foco em aplicação de soluções para a empresa"
+        />
+        <meta
+          property="og:image"
+          content="https://www.engseg.art.br/og_image.jpg"
+        ></meta>
+        <meta property="og:url" content="https://www.engseg.art.br/" />
+      </Head>
+
       <Header />
       <Component {...pageProps} />
       <Whatsapp />
